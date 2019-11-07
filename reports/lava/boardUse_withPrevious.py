206ec59337a2af39d18cd83378adf1d6b026f593
Pete Dyer <pete.dyer@arm.com>
2019-11-07 16:05:00 +0000

    Testing

--- reports/lava/boardUse_withPrevious.py
+++ reports/lava/boardUse_withPrevious.py
@@ -78,7 +78,7 @@ def main():
     parser.add_argument(
         "--pickle",
         type=str,
-        default="lava-farm-status.p",
+        default="lava-farm-status-pickle.html",
         nargs=None,
         help="Name of pickle file that contains previous/current status",
     )
